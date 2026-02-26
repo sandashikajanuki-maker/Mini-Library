8be2bb363344eac9573704774dd0bb35b8a9a808
Gaze <amodhkushan2005@gmail.com>
2026-02-26 12:24:13 +0530

    Added Navbar & Sidebar

---
+++ public/assets/js/navsidebar.js
@@ -0,0 +1,8 @@
+const toggleBtn = document.getElementById('toggle-btn');
+const sidebar = document.getElementById('sidebar');
+const mainContent = document.getElementById('main-content');
+
+toggleBtn.addEventListener('click', () => {
+    sidebar.classList.toggle('collapsed');
+    mainContent.classList.toggle('expanded');
+});
